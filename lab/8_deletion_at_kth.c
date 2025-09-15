002b9c4b814cd7de297fd71b5d3031c0cb1a2327
Mumin Islahi <islahimumin@gmail.com>
2025-09-15 08:31:12 +0530

    Update 8_deletion_at_kth.c

--- lab/8_deletion_at_kth.c
+++ lab/8_deletion_at_kth.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
 
+int main() {
+
+    int array[10];
+    printf("Number of elements to input\n");
+
+    int count,k;
+    scanf("%d",&count);
+
+
+    
+    if(count)
+    {
+
+        printf("Enter the elements of the array\n");
+        for(int i=0;i<count;i++)
+        {
+            scanf("%d", &array[i]);
+        }
+
+        printf("Which position needs to be deleted?");
+        scanf("%d",&k);
+
+        for (int i=(k-1);i<count;i++)
+        {
+            array[i]=array[i+1];   
+        }
+        count--;
+
+        for(int i=0;i<count;i++)
+        {
+            printf("%d\t",array[i]);
+        }
+    }
+
+    else 
+        printf("Not enough elements");
+        
+    return 0;
+}
